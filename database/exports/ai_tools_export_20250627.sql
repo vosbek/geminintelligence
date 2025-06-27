dba51401800832825d722adff9d359c9c4468b40
Hairston <HAIRSM2@nationwide.com>
2025-06-27 19:51:10 -0400

    run

--- database/exports/ai_tools_export_20250627.sql
+++ database/exports/ai_tools_export_20250627.sql
@@ -1,5 +1,5 @@
 \echo 'Populating ai_tools...'
-INSERT INTO "ai_tools" (id, name, description, github_url, stock_symbol, category, company_name, legal_company_name, status, run_status, last_run, created_at, updated_at) VALUES (1, 'Cursor', 'The AI Code Editor', 'https://github.com/getcursor/cursor', NULL, 'AI_IDE', 'Anysphere', 'Anysphere Inc.', 'active', 'processed', '2025-06-27T17:39:46.753079'::timestamp, '2025-06-27T17:37:37.571995'::timestamp, '2025-06-27T17:39:46.753953'::timestamp);
+INSERT INTO "ai_tools" (id, name, description, github_url, stock_symbol, category, company_name, legal_company_name, status, run_status, last_run, created_at, updated_at) VALUES (1, 'Cursor', 'The AI Code Editor', 'https://github.com/getcursor/cursor', NULL, 'AI_IDE', 'Anysphere', 'Anysphere Inc.', 'active', 'processed', '2025-06-27T19:49:20.315418'::timestamp, '2025-06-27T19:46:50.427328'::timestamp, '2025-06-27T19:49:20.316172'::timestamp);
 
 \echo 'Populating curated_snapshots...'
 \echo 'Populating data_sources...'
