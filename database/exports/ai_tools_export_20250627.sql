c05b5004044de0e3feff04c9fdd13ac18c49fcb8
Hairston <HAIRSM2@nationwide.com>
2025-06-27 14:37:09 -0400

    data changes

--- database/exports/ai_tools_export_20250627.sql
+++ database/exports/ai_tools_export_20250627.sql
@@ -1,5 +1,5 @@
 \echo 'Populating ai_tools...'
-INSERT INTO "ai_tools" (id, name, description, github_url, stock_symbol, category, status, run_status, last_run, created_at, updated_at) VALUES (1, 'Cursor', 'The AI Code Editor', 'https://github.com/getcursor/cursor', 'NULL', 'AI_IDE', 'active', 'processed', '2025-06-27T12:43:01.215263'::timestamp, '2025-06-27T12:40:57.340886'::timestamp, '2025-06-27T12:43:01.215944'::timestamp);
+INSERT INTO "ai_tools" (id, name, description, github_url, stock_symbol, category, status, run_status, last_run, created_at, updated_at) VALUES (1, 'Cursor', 'The AI Code Editor', 'https://github.com/getcursor/cursor', 'NULL', 'AI_IDE', 'active', 'processed', '2025-06-27T14:35:28.067506'::timestamp, '2025-06-27T14:32:52.709207'::timestamp, '2025-06-27T14:35:28.068146'::timestamp);
 
 \echo 'Populating curated_snapshots...'
 \echo 'Populating data_sources...'
